8d2e20b146c580f1c07b23f217d3ea39f077c396
Jesse <jesse.mashoana@gmail.com>
2024-11-20 12:50:00 +0200

    tired

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,96 +1,43 @@
-import {  View, Text, Pressable, StyleSheet, TextInput, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
-export default function RootLayout(){
+export default function RootLayout() {
   return (
     <SafeAreaView style={styles.container}>
-
-      <ScrollView contentContainerStyle={styles.ScrollView}>
-
-      <Text style={styles.title}>Form</Text>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="First Name" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Last Name" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Age" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Phone Number" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Email" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Password" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Confirm Password" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="House No." 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Street Name" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="City" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <View style={styles.form}>
-      <TextInput 
-      style={styles.input} 
-      placeholder="Province" 
-      placeholderTextColor={"white"}  
-      onChangeText={(text) => {console.log(text)}}/>
-      </View>
-      <Pressable style={styles.button}>
-        <Text style={styles.buttonText}>Press Here</Text>
-      </Pressable>
-      <StatusBar backgroundColor="#010789" style='light'/>
+      <ScrollView>
+        <Image
+        style={{ height: 200}} 
+        source={require("../assets/images/adaptive-icon.png")} />
+        <Image
+        style={{ height: 200,width: 200, resizeMode: "contain" }} 
+        source={require("../assets/images/adaptive-icon.png")} />
+        <Image
+        style={{ height: 200, resizeMode: "repeat" }} 
+        source={require("../assets/images/adaptive-icon.png")} />
+        <Image
+        style={{ height: 200, resizeMode: "cover" }} 
+        source={require("../assets/images/adaptive-icon.png")} />
+        <Image
+        style={{ width: 200, resizeMode: "stretch" }} 
+        source={require("../assets/images/adaptive-icon.png")} />
+        <Image
+        style={{ width: 800, height: 800 , resizeMode: "contain" }} 
+        onLoadStart={() => console.log("Loading")}
+        onLoadEnd={() => console.log("Loaded")}
+        source={{uri:"https://i.pinimg.com/736x/59/54/61/59546197baae43e5cd4612bbe1d4424d.jpg" }} />
       </ScrollView>
+      <StatusBar backgroundColor="#010789" style="light" />
     </SafeAreaView>
   );
 }
@@ -101,40 +48,4 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
   },
-  ScrollView: {
-gap:15,
-paddingVertical:20
-  },
-  title: {
-    color: "white",
-    fontSize: 24,
-    fontWeight: "700",
-    marginTop: 20,
-    textAlign: "center",
-    alignItems: "center"
-  },
-  input: {
-    backgroundColor: "transparent",
-    color: "white",
-    padding: 15,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "grey",
-    borderRadius: 10,
-    marginTop: 10,
-  },
-  button: {
-    backgroundColor: "#306A68",
-    padding: 15,
-    marginTop: 20,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-    textTransform: "uppercase",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-})
+});
