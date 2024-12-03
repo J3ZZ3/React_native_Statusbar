e5b4c24cb5c5e77e9d514ce47893978ca0dae388
Jesse <jesse.mashoana@gmail.com>
2024-12-03 16:02:56 +0200

    911

--- app/(auth)/(drawer)/_layout.js
+++ app/(auth)/(drawer)/_layout.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Drawer } from 'expo-router/drawer';
+import { Drawer } from 'expo-router';
 
 const _layout = () => {
   return (
-    <Drawer />
+  <Text>hello</Text>
   )
 }
+
 
 export default _layout
 
--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import { ActivityIndicator, View, Text } from 'react-native';
-
+import {Drawer} from 'expo-router/drawer';
 const StartPage = () => {
   return (
+    <Stack>
+    <Drawer/>
     <View style={{ flex: 1, justifyContent: 'center' }}>
         <Text>Loading...</Text>
       <ActivityIndicator size="large" color="#000000" />
     </View>
+    </Stack>
   );
 };
 
